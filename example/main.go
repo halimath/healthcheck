f92e12078f40c078a1c8e8effc50d9803489783d
Alexander Metzner <alexander.metzner@gmail.com>
2023-02-10 18:51:21 +0100

    doc: example

---
+++ example/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/halimath/healthcheck"
+)
+
+func main() {
+	h := healthcheck.New(nil)
+
+	h.AddCheck(healthcheck.CheckURL("http://localhost:1234/"))
+	h.AddCheckFunc(func(ctx context.Context) error {
+		// Add your check code here
+		return nil
+	})
+
+	http.Handle("/health/", http.StripPrefix("/health", h))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
